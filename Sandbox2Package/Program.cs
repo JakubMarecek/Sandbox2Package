fea21d128fc3bc97c5eca20a8ad99e8454a32d92
Jakub Mareček <jakubmarecek715@gmail.com>
2021-12-11 11:48:45 +0100

    tweaks

--- Sandbox2Package/Program.cs
+++ Sandbox2Package/Program.cs
@@ -6,7 +6,8 @@ namespace ConsoleApp1
 {
     class Program
     {
-        public static string version = "20211205-2000";
+        public static string version = "20211211-1200";
+        public static string outFile = "Custom Sandbox Creation.a3";
 
         static void Main(string[] args)
         {
@@ -122,54 +123,85 @@ namespace ConsoleApp1
         {
             Dictionary<string, List<XElement>> replaces = new();
 
+            XElement xReplaces = new("Replaces");
+            void WriteReplaces()
+            {
+                foreach (KeyValuePair<string, List<XElement>> pair in replaces)
+                {
+                    XElement xReplace = new("Replace", new XAttribute("RequiredFile", pair.Key));
+                    XElement CSectorSpawnCategory = new("object", new XAttribute("hash", "464A7F75"));
+                    XElement MissionLayer = new("object", new XAttribute("hash", "494C09F2"));
+                    MissionLayer.Add(new XElement("primaryKey", new XAttribute("hash", "B88F49FD"), "6400000000000000"));
+
+                    foreach (XElement entity in pair.Value)
+                        MissionLayer.Add(entity);
+
+                    CSectorSpawnCategory.Add(MissionLayer);
+                    xReplace.Add(CSectorSpawnCategory);
+                    xReplaces.Add(xReplace);
+                }
+            }
+
             int cnt = 0;
             string[] lines = File.ReadAllLines(sandboxSavedFile);
             foreach (string line in lines)
             {
-                string[] splited = line.Split(',');
-                if (splited.Length > 1)
+                if (line != "" && !line.StartsWith(";"))
                 {
-                    string conf = GetWorldSectorConfig("fc5_main", splited[1], splited[2]);
-
-                    long entityID = (DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond) + cnt;
-
-                    XElement entity = new("object", new XAttribute("name", "Entity"), new XAttribute("addNode", "1"));
-                    entity.Add(new XElement("field", $@"entityarchetypeslibrary\{splited[0]}.ark.fcb", new XAttribute("name", "text_hidArchetypeResId"), new XAttribute("type", "String")));
-                    entity.Add(new XElement("field", $@"entityarchetypeslibrary\{splited[0]}.ark.fcb", new XAttribute("name", "hidArchetypeResId"), new XAttribute("type", "ComputeHash64")));
-                    entity.Add(new XElement("field", $"World:ArmanIII:Sandbox:Entity{entityID}", new XAttribute("hash", "1DD79FAD"), new XAttribute("type", "String")));
-                    entity.Add(new XElement("field", $"SandboxEntity{entityID}", new XAttribute("name", "hidName"), new XAttribute("type", "String")));
-                    entity.Add(new XElement("field", entityID.ToString(), new XAttribute("name", "disEntityId"), new XAttribute("type", "Id64")));
-                    entity.Add(new XElement("field", $"{splited[1]},{splited[2]},{splited[3]}", new XAttribute("name", "hidPos"), new XAttribute("type", "Vector3")));
-                    entity.Add(new XElement("field", $"{splited[4]},{splited[5]},{splited[6]}", new XAttribute("name", "hidAngles"), new XAttribute("type", "Vector3")));
-                    entity.Add(new XElement("field", $"{splited[1]},{splited[2]},{splited[3]}", new XAttribute("name", "hidPos_precise"), new XAttribute("type", "Vector3")));
-                    entity.Add(new XElement("field", "True", new XAttribute("name", "SpawnThreadSafe"), new XAttribute("type", "Boolean")));
-                    entity.Add(new XElement("field", new XAttribute("name", "ArchetypeResDepList"), new XElement("Resource", new XAttribute("ID", $@"entityarchetypeslibrary\{splited[0]}.ark.fcb"))));
-
-                    /*XElement components = new("object", new XAttribute("name", "Components"));
+                    string[] splited = line.Split(',');
+                    if (splited.Length > 1)
                     {
-                        XElement navtComp = new("object", new XAttribute("name", "CNavLinkComponent"));
-                        {
-                            navtComp.Add(new XElement("field", "False", new XAttribute("name", "bActivated"), new XAttribute("type", "Boolean")));
-                        }
-                        components.Add(navtComp);
+                        string conf = GetWorldSectorConfig("fc5_main", splited[1], splited[2]);
 
-                        XElement eventComp = new("object", new XAttribute("name", "CEventComponent"));
+                        long entityID = (DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond) + cnt;
+
+                        XElement entity = new("object", new XAttribute("name", "Entity"), new XAttribute("addNode", "1"));
+                        entity.Add(new XElement("field", $@"entityarchetypeslibrary\{splited[0]}.ark.fcb", new XAttribute("name", "text_hidArchetypeResId"), new XAttribute("type", "String")));
+                        entity.Add(new XElement("field", $@"entityarchetypeslibrary\{splited[0]}.ark.fcb", new XAttribute("name", "hidArchetypeResId"), new XAttribute("type", "ComputeHash64")));
+                        entity.Add(new XElement("field", $"World:ArmanIII:Sandbox:Entity{entityID}", new XAttribute("hash", "1DD79FAD"), new XAttribute("type", "String")));
+                        entity.Add(new XElement("field", $"SandboxEntity{entityID}", new XAttribute("name", "hidName"), new XAttribute("type", "String")));
+                        entity.Add(new XElement("field", entityID.ToString(), new XAttribute("name", "disEntityId"), new XAttribute("type", "Id64")));
+                        entity.Add(new XElement("field", $"{splited[1]},{splited[2]},{splited[3]}", new XAttribute("name", "hidPos"), new XAttribute("type", "Vector3")));
+                        entity.Add(new XElement("field", $"{splited[4]},{splited[5]},{splited[6]}", new XAttribute("name", "hidAngles"), new XAttribute("type", "Vector3")));
+                        entity.Add(new XElement("field", $"{splited[1]},{splited[2]},{splited[3]}", new XAttribute("name", "hidPos_precise"), new XAttribute("type", "Vector3")));
+                        entity.Add(new XElement("field", "True", new XAttribute("name", "SpawnThreadSafe"), new XAttribute("type", "Boolean")));
+                        entity.Add(new XElement("field", new XAttribute("name", "ArchetypeResDepList"), new XElement("Resource", new XAttribute("ID", $@"entityarchetypeslibrary\{splited[0]}.ark.fcb"))));
+
+                        /*XElement components = new("object", new XAttribute("name", "Components"));
                         {
-                            eventComp.Add(new XElement("field", "False", new XAttribute("name", "hidHasAliasName"), new XAttribute("type", "Boolean")));
-                            eventComp.Add(new XElement("object", new XAttribute("name", "hidLinks")));
+                            XElement navtComp = new("object", new XAttribute("name", "CNavLinkComponent"));
+                            {
+                                navtComp.Add(new XElement("field", "False", new XAttribute("name", "bActivated"), new XAttribute("type", "Boolean")));
+                            }
+                            components.Add(navtComp);
+
+                            XElement eventComp = new("object", new XAttribute("name", "CEventComponent"));
+                            {
+                                eventComp.Add(new XElement("field", "False", new XAttribute("name", "hidHasAliasName"), new XAttribute("type", "Boolean")));
+                                eventComp.Add(new XElement("object", new XAttribute("name", "hidLinks")));
+                            }
+                            components.Add(eventComp);
                         }
-                        components.Add(eventComp);
+                        entity.Add(components);*/
+
+                        if (!replaces.ContainsKey(conf))
+                            replaces.Add(conf, new List<XElement>());
+
+                        replaces[conf].Add(entity);
+
+                        cnt++;
                     }
-                    entity.Add(components);*/
+                }
 
-                    if (!replaces.ContainsKey(conf))
-                        replaces.Add(conf, new List<XElement>());
-
-                    replaces[conf].Add(entity);
-
-                    cnt++;
+                if (line.StartsWith(";"))
+                {
+                    WriteReplaces();
+                    xReplaces.Add(new XComment(line.Replace(";", "")));
+                    replaces = new();
                 }
             }
+
+            WriteReplaces();
 
 
             XDocument xInfoReplaceXML = new(new XDeclaration("1.0", "utf-8", "yes"));
@@ -177,22 +209,6 @@ namespace ConsoleApp1
             xInfoReplace.Add(new XElement("Games", new XElement("Game", "FC5")));
             xInfoReplace.Add(new XElement("Name", "Custom Sandbox Creation"));
             xInfoReplace.Add(new XElement("Description", "Custom placed objects using Sandbox."));
-
-            XElement xReplaces = new("Replaces");
-            foreach (KeyValuePair<string, List<XElement>> pair in replaces)
-            {
-                XElement xReplace = new("Replace", new XAttribute("RequiredFile", pair.Key));
-                XElement CSectorSpawnCategory = new("object", new XAttribute("hash", "464A7F75"));
-                XElement MissionLayer = new("object", new XAttribute("hash", "494C09F2"));
-                MissionLayer.Add(new XElement("primaryKey", new XAttribute("hash", "B88F49FD"), "6400000000000000"));
-
-                foreach (XElement entity in pair.Value)
-                    MissionLayer.Add(entity);
-
-                CSectorSpawnCategory.Add(MissionLayer);
-                xReplace.Add(CSectorSpawnCategory);
-                xReplaces.Add(xReplace);
-            }
 
             xInfoReplace.Add(xReplaces);
             xInfoReplaceXML.Add(xInfoReplace);
@@ -202,7 +218,10 @@ namespace ConsoleApp1
             ms.Seek(0, SeekOrigin.Begin);
 
 
-            ZipArchive zip = ZipFile.Open(outDir + "Custom Sandbox Creation.a3", ZipArchiveMode.Create);
+            if (File.Exists(outDir + outFile))
+                File.Delete(outDir + outFile);
+
+            ZipArchive zip = ZipFile.Open(outDir + outFile, ZipArchiveMode.Create);
             ZipArchiveEntry zipInfo = zip.CreateEntry("info_replace.xml");
             using (Stream entryStream = zipInfo.Open())
             {
